35002683b26c650ce669ba06ae3631076ca0d835
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2020-02-01 14:18:33 +0100

    Create it.json (#54)
    * Create it.json
    
    * Create it.js

---
+++ l10n/it.js
@@ -0,0 +1,29 @@
+OC.L10N.register(
+    "files_mindmap",
+    {
+        "New mind map file": "Nuova mappa mentale",
+        "New mind map.km": "Nuova mappa mentale.km",
+        "Main Topic": "Soggetto Principale",
+        "Saving...": "Salvataggio...",
+        "This file is not a valid mind map file and may cause file corruption if you continue editing.": "Questo file non è un file di mappa mentale valido e può causare danni al file se si continua la modifica.",
+        "Load file fail!": "Caricamento file fallito",
+        "File Saved": "File Salvato",
+        "Save failed": "Salvataggio fallito",
+        "Export": "Esporta",
+        "Export to PNG": "Esporta in PNG",
+        "Export to SVG": "Esporta in SVG",
+        "You are not permission to write this file":"Non sei autorizzato a scrivere questo file",
+        "File not found":"File non trovato",
+        "The file is locked.":"Il file è bloccato.",
+        "Share not found":"Condivisione non trovata",
+        "You are not authorized to open this share":"Non sei autorizzato ad aprire questa condivisione",
+        "Could not write to file.":"Impossibile scrivere sul file.",
+        "Export to Markdown":"Esporta in Markdown",
+        "Export to Text":"Export to Text":"Esporta in Testo",
+        "Export to PDF":"Esporta in PDF",
+        "Save":"Salva",
+        "The file has not been saved. Is it saved?": "Il file non è stato salvato. È salvato?",
+        "Unsaved file": "File non salvato"
+    },
+    "nplurals=1; plural=0;"
+  }
