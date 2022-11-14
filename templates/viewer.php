25ecb0d2dac3d40c49bd6ef9dedca50d9eec38f4
ACTom <i@actom.me>
2022-11-14 22:03:23 +0800

    Support NC 25

--- templates/viewer.php
+++ templates/viewer.php
@@ -2,7 +2,7 @@
   /** @var array $_ */
   /** @var OCP\IURLGenerator $urlGenerator */
   $urlGenerator = $_['urlGenerator'];
-  $version = \OCP\App::getAppVersion('files_mindmap');
+  $version = \OC::$server->getAppManager()->getAppVersion('files_mindmap');
   $lang = $_['lang'];
   if (method_exists(\OC::$server, 'getContentSecurityPolicyNonceManager')) {
       $nonce = \OC::$server->getContentSecurityPolicyNonceManager()->getNonce();
