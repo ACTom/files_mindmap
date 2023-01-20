b33a4640f9a184f97843414ab0c00a92e50b333a
Mateusz Bryl <brylek@windowslive.com>
2023-01-20 21:12:23 +0100

    correct pl.json and make polish version pl.json

--- l10n/pl.js
+++ l10n/pl.js
@@ -1,3 +1,4 @@
+//I try dont use polish diacritical mark in .js file
 OC.L10N.register(
     "files_mindmap", 
     {
@@ -5,27 +6,27 @@ OC.L10N.register(
         "New mind map.km": "Nowa mapa_mysli.km",
         "Main Topic": "Glowny temat",
         "Saving...": "Zapisywanie...",
-        "This file is not a valid mind map file and may cause file corruption if you continue editing.": "Ten plik nie jest poprawną mind mapa mozliwe problemy przy kontynuowaniu pracy",
-        "Load file fail!": "Blad zapisywania pliku",
+        "This file is not a valid mind map file and may cause file corruption if you continue editing.": "Ten plik nie jest poprawna mapa mysli mozliwe problemy przy kontynuowaniu pracy",
+        "Load file fail!": "Blad zapisywania pliku!",
         "File Saved": "Zapisano plik",
-        "Save failed": "Zapis nieudany",
+        "Save failed": "Zapis pliku nieudany",
         "Export": "Wyeksportuj",
         "Export to PNG": "Wyeksportuj do PNG",
         "Export to SVG": "Wyeksportuj do SVG",
         "You are not permission to write this file": "Nie masz praw do zapisu tego pliku",
         "File not found": "Nie znaleziono pliku",
         "The file is locked.": "Plik zablokowany",
-        "Share not found": "Nie znaleziono zasobu udostępnionego",
+        "Share not found": "Nie znaleziono zasobu udostepnionego",
         "You are not authorized to open this share": "Nie masz prawa do otwarcia tego zasobu udostepnionego",
         "Could not write to file.": "Nie mozna zapisac pliku",
         "Export to Markdown": "Wyekspotuj do Markdown'u",
         "Export to Text": "Wyeksportuj do Tekstu",
         "Export to PDF": "Wyeksportuj do PDF",
         "Save": "Zapisano",
-        "The file has not been saved. Is it saved?": "Nie zapisano pliku czy chcesz go zapisac",
+        "The file has not been saved. Is it saved?": "Nie zapisano pliku czy chcesz go zapisac?",
         "Unsaved file": "Niezapisany plik",
         "AutoSave": "Zapis automatyczny",
-        "Does not support saving {extension} files.": "Nie wspieraw zapisu w formacie {extension}.",
+        "Does not support saving {extension} files.": "Brak wsparcia dla zapisu w formacie {extension}.",
         "Unsupported file type: {mimetype}": "Nie wspierany typ pliku : {mimetype}"
     }, "nplurals=1; plural=0;"
 );
