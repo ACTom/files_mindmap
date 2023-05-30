185d77eaec7f3f6364feab35082c5aece6890df2
Mateusz Bryl <60017069+brylos1@users.noreply.github.com>
2023-05-30 06:05:39 +0200

    Polish version (#183)
    * Create pl.json
    
    * correct pl.json and make polish version pl.json

---
+++ l10n/pl.js
@@ -0,0 +1,32 @@
+//I try dont use polish diacritical mark in .js file
+OC.L10N.register(
+    "files_mindmap", 
+    {
+        "New mind map file": "Nowa mapa_mysli",
+        "New mind map.km": "Nowa mapa_mysli.km",
+        "Main Topic": "Glowny temat",
+        "Saving...": "Zapisywanie...",
+        "This file is not a valid mind map file and may cause file corruption if you continue editing.": "Ten plik nie jest poprawna mapa mysli mozliwe problemy przy kontynuowaniu pracy",
+        "Load file fail!": "Blad zapisywania pliku!",
+        "File Saved": "Zapisano plik",
+        "Save failed": "Zapis pliku nieudany",
+        "Export": "Wyeksportuj",
+        "Export to PNG": "Wyeksportuj do PNG",
+        "Export to SVG": "Wyeksportuj do SVG",
+        "You are not permission to write this file": "Nie masz praw do zapisu tego pliku",
+        "File not found": "Nie znaleziono pliku",
+        "The file is locked.": "Plik zablokowany",
+        "Share not found": "Nie znaleziono zasobu udostepnionego",
+        "You are not authorized to open this share": "Nie masz prawa do otwarcia tego zasobu udostepnionego",
+        "Could not write to file.": "Nie mozna zapisac pliku",
+        "Export to Markdown": "Wyekspotuj do Markdown'u",
+        "Export to Text": "Wyeksportuj do Tekstu",
+        "Export to PDF": "Wyeksportuj do PDF",
+        "Save": "Zapisano",
+        "The file has not been saved. Is it saved?": "Nie zapisano pliku czy chcesz go zapisac?",
+        "Unsaved file": "Niezapisany plik",
+        "AutoSave": "Zapis automatyczny",
+        "Does not support saving {extension} files.": "Brak wsparcia dla zapisu w formacie {extension}.",
+        "Unsupported file type: {mimetype}": "Nie wspierany typ pliku : {mimetype}"
+    }, "nplurals=1; plural=0;"
+);
