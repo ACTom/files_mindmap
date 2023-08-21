7eba4ac8bee4c4daa6b8c6b0939dfd165e1eded3
Sebastien Marinier <sebastien.marinier@arawa.fr>
2023-08-21 09:19:15 +0200

    Fix #184 : move from app.php to NC 20 Application.php

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -3,23 +3,57 @@
 namespace OCA\Files_MindMap\AppInfo;
 
 use OC\Files\Type\Detection;
+use OCA\Files\Event\LoadAdditionalScriptsEvent;
+use OCA\Files_MindMap\Listener\LoadAdditionalScripts;
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\Http\ContentSecurityPolicy;
+use OCP\EventDispatcher\IEventDispatcher;
+use OCP\Files\IMimeTypeDetector;
+use OCP\Security\CSP\AddContentSecurityPolicyEvent;
 
-class Application extends App {
+
+class Application extends App implements IBootstrap {
     const APPNAME = 'files_mindmap';
 
 	public function __construct(array $urlParams = array()) {
 		parent::__construct(self::APPNAME, $urlParams);
     }
-    
+
 
 	public function registerProvider() {
 		$container = $this->getContainer();
 
 		// Register mimetypes
 		/** @var Detection $detector */
-		$detector = $container->query(\OCP\Files\IMimeTypeDetector::class);
+		$detector = $container->get(IMimeTypeDetector::class);
 		$detector->getAllMappings();
 		$detector->registerType('km','application/km');
+	}
+
+	public function register(IRegistrationContext $context): void {
+
+		$context->registerEventListener(
+			LoadAdditionalScriptsEvent::class,
+			LoadAdditionalScripts::class
+		);
+	}
+
+	public function boot(IBootContext $context): void {
+		$this->registerProvider();
+
+		$context->injectFn([$this, 'registerEventsSecurity']);
+	}
+
+	public function registerEventsSecurity(IEventDispatcher $dispatcher): void {
+		$dispatcher->addListener(AddContentSecurityPolicyEvent::class, function (AddContentSecurityPolicyEvent $e) {
+			$policy = new ContentSecurityPolicy();
+			$policy->addAllowedChildSrcDomain("'self'");
+			$policy->addAllowedFrameDomain("data:");
+			$e->addPolicy($policy);
+		});
+
 	}
 }
